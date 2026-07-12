72dcea29ba327734a895256416970f5773db38e0
Koustubh Sinha <98702398+Kola0D246@users.noreply.github.com>
2026-07-13 04:19:18 +0530

    feat: added particle background (#6)

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from '@/components/public/Header';
 import Footer from '@/components/public/Footer';
+import ParticleBackground from '@/components/shared/ParticleBackground';
 
 export default function PublicLayout() {
     return (
         <>
+        <ParticleBackground />
         <Header />
         <main><Outlet /></main>
         <Footer />
