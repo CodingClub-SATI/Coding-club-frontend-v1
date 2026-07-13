30f2176a6d761a3b07b95c144f373ac5655f36e5
Kola0D246 <koustubhsinhapolytecnic@gmail.com>
2026-07-14 01:00:45 +0530

    fix:Nuke App.jsx wrapper and render application tree in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from '@/App.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import router from './router';
+import { RouterProvider } from 'react-router-dom';
 
+// Global styles
 import './styles/global.css';
 import './styles/utilities.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
