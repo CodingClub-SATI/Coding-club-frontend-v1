6f2ab45506146fa6256d9914f3fb948a3c0b8184
Koustubh Sinha <98702398+Kola0D246@users.noreply.github.com>
2026-07-14 03:46:47 +0530

    fix/particle background (#7)
    * fix:move particle background and fix its imports
    
    * fix:reconfigure for efficient visuals and background

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Header from '@/components/public/Header';
 import Footer from '@/components/public/Footer';
-import ParticleBackground from '@/components/shared/ParticleBackground';
+import ParticleBackground from '@/components/public/ParticleBackground';
 
 export default function PublicLayout() {
     return (
