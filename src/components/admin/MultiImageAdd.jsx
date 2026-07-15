3491c7a7be505108c86b223847f773c13c700b7e
Kola0D246 <koustubhsinhapolytecnic@gmail.com>
2026-07-16 05:08:13 +0530

    FS: add admin component jsx

---
+++ src/components/admin/MultiImageAdd.jsx
@@ -0,0 +1,17 @@
+import Button from '@/components/shared/Button';
+
+export default function MultiImageAdd({ onAdd }) {
+  const inputRef = useRef(null);
+  const handleFiles = (files) => {
+    const photos = Array.from(files).map(f => ({ src: URL.createObjectURL(f), caption: f.name.replace(/\.[^.]+$/, '') }));
+    if (photos.length) onAdd(photos);
+  };
+  return (
+    <>
+      <input ref={inputRef} type="file" accept="image/*" multiple hidden onChange={(e) => { handleFiles(e.target.files); e.target.value = ''; }} />
+      <button className="btn-outline" onClick={() => inputRef.current?.click()}>
+        <Plus size={16} /> Add Photos
+      </button>
+    </>
+  );
+}
